5fffa9001aa518a56d8a7a5a02728dc91487525a
Martin Karugaba <martinkarugaba21@gmail.com>
2023-05-22 09:45:51 +0300

    Add edit functionality

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -28,7 +28,7 @@ const InputField: React.FC<Props> = ({
           value={todo}
           onChange={(e) => setTodo(e.target.value)}
           placeholder="Enter a task"
-          className="p-2 rounded"
+          className="p-2 px-3 rounded"
         />
         <button type="submit" className="ml-2 bg-orange-400 text-white font-medium rounded p-2 px-4">Go</button>
       </div>
--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
@@ -13,6 +14,9 @@ const SingleTodo: React.FC<Props> = ({
   todos,
   setTodos,
 }: Props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
   const handleDone = (id: number) => {
     setTodos(
       todos.map((todo) =>
@@ -25,22 +29,61 @@ const SingleTodo: React.FC<Props> = ({
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id ? { ...todo, todo: editTodo } : todo;
+      })
+    );
+    setEdit(false);
+  };
+
   return (
-    <form className="mt-4">
-      <div className="w-[500px] flex justify-between items-center py-2 rounded border">
-        {todo.isDone ? (
+    <form className="mt-4" onSubmit={(e) => handleEdit(e, todo.id)}>
+      <div className="w-[500px] flex justify-between items-center p-2 px-3 rounded bg-orange-400">
+        {edit ? (
+          <>
+            <input
+              type="text"
+              value={editTodo}
+              onChange={(e) => setEditTodo(e.target.value)}
+              className="flex-1 rounded px-3 py-1  outline-none"
+            />
+            <button type="submit" className="mr-4">
+              Done
+            </button>
+          </>
+        ) : todo.isDone ? (
           <s className="text-white line-through">{todo.todo}</s>
         ) : (
           <p className="text-white">{todo.todo}</p>
         )}
+
         <div className="w-[80px] flex justify-between items-center">
-          <span>
+          {/* edit button */}
+          <span
+            className="text-white text-xl cursor-pointer"
+            onClick={() => {
+              if (!edit && !todo.isDone) {
+                setEdit(!edit);
+              }
+            }}
+          >
             <AiFillEdit />
           </span>
-          <span onClick={() => handleDelete(todo.id)}>
+          {/* delete button */}
+          <span
+            onClick={() => handleDelete(todo.id)}
+            className="text-red-600 text-xl cursor-pointer"
+          >
             <AiFillDelete />
           </span>
-          <span onClick={() => handleDone(todo.id)}>
+          {/* Done button */}
+          <span
+            onClick={() => handleDone(todo.id)}
+            className="text-green-600 text-xl cursor-pointer"
+          >
             <MdDone />
           </span>
         </div>
